9fff9ac4676a586725c106ab15859b4d31256e75
Robert Haji <roberthadji66@gmail.com>
2025-06-22 22:20:51 +0300

    lab_done

--- server/app.py
+++ server/app.py
@@ -20,19 +20,37 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+
+    response = make_response(bakeries, 200)
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    if not bakery:
+        return make_response({"error": "Bakery not found"}, 404)
+
+    response = make_response(bakery.to_dict(), 200)
+    return response
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = [
+        bg.to_dict() for bg in BakedGood.query.order_by(BakedGood.price.desc()).all()
+    ]
+    response = make_response(baked_goods, 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    bg = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if not bg:
+        return make_response({"error": "No baked goods found"}, 404)
+
+    response = make_response(bg.to_dict(), 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
